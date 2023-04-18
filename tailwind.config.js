f7f99951e3e34b6af88cb04a69b604abfb366236
dohpark <dohpark1129@gmail.com>
2023-04-19 02:52:57 +0900

    feat: #5 앱 레이아웃 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    screens: {
+      xs: { max: '640px' },
+      // => @media (max-width: 640px) { ... }
+
+      sm: { min: '640px' },
+      // => @media (min-width: 640px) { ... }
+
+      md: { min: '768px' },
+      // => @media (min-width: 768px) { ... }
+
+      lg: { min: '1024px' },
+      // => @media (min-width: 1024px) { ... }
+
+      xl: { min: '1280px' },
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': { min: '1536px' },
+      // => @media (min-width: 1536px) { ... }
+    },
+    maxWidth: {
+      40: '40rem',
+    },
   },
   plugins: [],
 };
